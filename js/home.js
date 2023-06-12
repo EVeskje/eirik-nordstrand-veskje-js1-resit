5ae61296a5397591228dbbe9b7086ccedc852cbf
Eirik Nordstrand Veskje <eirik.nv@hotmail.com>
2023-06-12 21:52:19 +0200

    Added HTML, CSS & JS

---
+++ js/home.js
@@ -0,0 +1,37 @@
+const jokesContainer = document.querySelector(".jokes-container");
+const jokeUrl = "https://api.noroff.dev/api/v1/jokes";
+
+let jokes = [];
+let type = "all";
+
+async function getAllJokes() {
+  try {
+    const response = await fetch(jokeUrl);
+    jokes = await response.json();
+
+    displayJokes(type);
+  } catch (error) {
+    alert("No more Funny Fiesta for you...");
+  }
+}
+
+getAllJokes();
+
+function displayJokes(type) {
+  const jokesToDisplay = getJokes(type);
+  jokesContainer.innerHTML = "";
+
+  for (let i = 0; i < jokesToDisplay.length; i++) {
+    if (i === 20) break;
+    const joke = jokesToDisplay[i];
+    jokesContainer.innerHTML += `<div class="card"><div class="card-type">${joke.type}</div>
+                               <div class="card-setup">${joke.setup}</div>
+                               <a href="joke.html?id=${joke.id}" class="card-punchline"><h3>Get punchline</h3></a></div></div>`;
+  }
+}
+
+function getJokes(type) {
+  if (type !== "all") return jokes.filter((joke) => joke.type === type);
+
+  return jokes;
+}
---
+++ js/joke.js
@@ -0,0 +1,37 @@
+const container = document.querySelector(".punchline-container");
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+console.log(id);
+
+const url = `https://api.noroff.dev/api/v1/jokes/${id}`;
+let joke;
+console.log(url);
+
+async function fetchJoke() {
+  try {
+    const response = await fetch(url);
+    joke = await response.json();
+    console.log(joke);
+    createHtml(joke);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchJoke();
+
+function createHtml(joke) {
+  container.innerHTML = `<div class="card">
+    <h1 class="card-type">${joke.type}</h1>
+    <div class="card-joke">${joke.setup}</div>
+    <div id="punchline">
+      <button class="punchline" onclick="showPunchline()">Show punchline</button>
+    </div>
+  </div>`;
+}
+
+function showPunchline() {
+  const element = document.getElementById("punchline");
+  element.innerHTML = `<div class="punchline">${joke.punchline}</div>`;
+}
